f5383bff2ead2aeb9c7ccbfbbfdf2babe7d49beb
JoaoAfranio <joaoafraniorosa@hotmail.com>
2022-10-04 21:43:01 -0300

    fixing layout seats

--- src/components/Seats.js
+++ src/components/Seats.js
@@ -2,20 +2,19 @@ import styled from "styled-components";
 
 export default function Seats({listSeats}) {
 
-    if(listSeats === undefined) <h1>Carregando...</h1>;
-
     return (
         <BoxSeats>
-            {listSeats.map((s) => <Seat>{s.name}</Seat>)}
+            {listSeats?.map((s) => <Seat>{s.name}</Seat>)}
         </BoxSeats>
     );
 }
 
 const BoxSeats = styled.div`
     display: flex;
+    justify-content: center;
     flex-wrap: wrap;
 
-    gap: 5px
+    gap: 18px 8px
 `;
 
 const Seat = styled.div`
@@ -23,10 +22,16 @@ const Seat = styled.div`
     width: 26px;
     border-radius: 12px;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    display: grid;
+    grid-template-columns: repeat(1fr, 10);
 
-    color: #808F9D;
+    align-items: center;
+    justify-content: center;
+
     background-color: #C3CFD9;
+
+    border: 1px solid #808F9D;
+
+    font-size: 11px;
+
 `;
